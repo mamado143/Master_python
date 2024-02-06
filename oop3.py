c8b28b674313981befebccd0018fd4b0070f768f
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2024-02-06 17:00:13 +0300

    Reconsile

--- oop3.py
+++ oop3.py
@@ -1,14 +1,23 @@
 # oop3.py
 class Members:
+    not_allowed_names = ["Hell", "Shit", "Wasmo"]
+    users_num = 0
+
+
+
     def __init__(self, first_name, middle_name, last_name, gender):
         self.fname = first_name
         self.age = 20
         self.mname = middle_name
         self.lname = last_name
         self.gender = gender
+        Members.users_num += 1  #Members.users
     
     def full_name(self):
-        return f"{self.fname} {self.mname} {self.lname} {self.gender}"
+        if self.fname in Members.not_allowed_names:
+            raise ValueError("Name Not Allowed")
+        else:
+            return f"{self.fname} {self.mname} {self.lname} {self.gender}"
     
     def initials(self):
         return f"{self.fname[0]}.{self.mname[0]}.{self.lname[0]}."
@@ -22,9 +31,15 @@ class Members:
             return f"{self.fname}"
     
     def get_all_info(self):
-        return f"{self.name_with_title()}\n, Your Full Name is  {self.full_name()}\n"
+        return f"{self.name_with_title()}\n\nYour Full Name is  {self.full_name()}\n"
+
+    def delete_user(self):
+        pass
+
+print(Members.users_num)
 member_one = Members("Mido", "Dahir", "Mohamoud", "Male")
 member_two = Members("Hamda", "Saciid", "Mohamoud", "Female")
 
 print(member_one.get_all_info())
 print(member_two.get_all_info())
+print(Members.users_num )
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,2 @@
-print(member_one.name_with_title())
-# print(member_two.name_with_title())
+print(member_one.get_all_info())
+print(member_two.get_all_info())
