9fa9812d915bc6e9b109307d0e2e244fa96719de
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2024-02-06 19:35:26 +0300

    add eat method in sub class

--- Inheritance.py
+++ Inheritance.py
@@ -10,9 +10,16 @@ class Apple(Food):
     def __init__(self, name, color):
         super().__init__(name, color)
         print(f"{self.name} is from sub class and it's color is {self.color}")
+    
+    def get_from_Tree(self):
+        print("get From sub class")
+    def eat(self):
+        print("eat from sub class")
 
 food_one = Food("Apple", "Red")
 
 food_two = Apple("Banana", "Yellow")
 food_two.eat()
+food_one.eat()
 print(food_one)
+food_two.get_from_Tree()
